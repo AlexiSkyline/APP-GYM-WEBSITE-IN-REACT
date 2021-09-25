4c721122cb6183a3a40304d7da54cb875e51cf7d
AlexiSkyline <ilegal_sprite@outlook.com>
2021-09-25 00:23:21 -0500

    we create and edit our Contact component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { About } from './Components/About';
+import { Contact } from './Components/Contact';
 import { Feature } from './Components/Feature';
 import { Header } from './Components/Header';
 import { NavBar } from './Components/NavBar';
@@ -13,6 +14,7 @@ function App() {
       <Feature />
       <Offer />
       <About />
+      <Contact />
     </div>
   );
 }
---
+++ src/Components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Contact = () => {
+    return (
+        <div id='contact'>
+            <h1>CONTACT US</h1>
+
+            <form>
+                <input type='text' placeholder='Full Name' required/>
+                <input type="email" placeholder='Type Your E-Mail' required/>
+                <textarea placeholder='Write Herr.......' name='message'></textarea>
+                <input type='submit' value='Send'/>
+            </form>
+            
+        </div>
+    )
+}
