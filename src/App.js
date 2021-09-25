3521d127d37ceebae46c9ce5ba1ff7201f341079
AlexiSkyline <ilegal_sprite@outlook.com>
2021-09-24 23:27:29 -0500

    we create and edit our Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Header } from './Components/Header';
 import { NavBar } from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Header />
     </div>
   );
 }
